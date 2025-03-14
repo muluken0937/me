cce86e1a060dd3eeeeadea9fa37947784161a9b8
muluken <mulukenjenber@gmail.com>
2025-03-14 22:44:20 +0300

    edit about

--- AQPortfolio/src/components/aboutMeSection/AboutMeText.jsx
+++ AQPortfolio/src/components/aboutMeSection/AboutMeText.jsx
@@ -5,13 +5,12 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p>
-        I’m Abdelrahman, a web developer instructor with a passion for teaching
-        and coding. I specialize in React and front-end development, helping
-        students build real-world projects and master modern web technologies. I
-        also run a YouTube channel, CodeNest, where I create tutorials and
-        courses to guide aspiring developers in their journey toward successful
-        careers in tech. Outside of coding, I enjoy continuous learning and
-        sharing knowledge to inspire others to achieve their goals.
+        I’m muluken jenber,I previously worked at{" "}
+        <strong>Scitech Valley</strong>, where I contributed to ERP and sales
+        performance tracking systems. Currently, I’m part of{" "}
+        <strong>EthixDev</strong>, focusing on both web and mobile development,
+        using technologies like{" "}
+        <strong>React, React Native, Node.js, Python, and Django</strong>.
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
         <Link
